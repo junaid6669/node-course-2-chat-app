c551ad57d6ec04d446de2191578fa4f1a5dcaf7a
Junaid Ali <junaidali0001@gmail.com>
2018-07-24 13:53:22 +0500

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -16,20 +16,35 @@ app.use(express.static(publicPath));
 io.on('connection', (socket)=>{
   console.log('New User Connected');
 // socket.emit emits to a single connection
-  // socket.emit('newMessage', {
-  //   from : "Hassan",
-  //   text : "Hey, What's going on",
-  //   createAt : 123
-  // });
+  socket.emit('newMessage', {
+    from : "Hassan",
+    text : "Hey, What's going on",
+    createdAt:new Date().getTime()
+
+  });
+
+
+  socket.broadcast.emit('newMessage', {
+    from:'Admin',
+    text:'New User Joined',
+    createdAt:new Date().getTime()
+  });
+
 
   socket.on('createMessage', (message)=>{
     console.log('Create Message', message);
     //io.emit emits to every single connected connection
-    io.emit('newMessage', { 
+    io.emit('newMessage', {
       from:message.from,
       to:message.text,
       createdAt:new Date().getTime()
     });
+
+    // socket.broadcast.emit('newMessage', {
+    //   from:message.from,
+    //   text:message.text,
+    //   createdAt:new Date().getTime()
+    //});
   });
 
   socket.on('disconnect', ()=>{
